78d57a5693c166f460ac8911416adc85dd3a6662
Yuliya <filatovajulia@rambler.ru>
2022-09-17 21:49:43 +0300

    add Ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -3,32 +3,32 @@
 
 Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
-GetArrayRandomNumbers(numbers);
+int[] array = new int[size];
+GetArrayRandomNumbers(array);
 Console.WriteLine("Массив: ");
-PrintArray(numbers);
+PrintArray(array);
 int count = 0;
 
-void GetArrayRandomNumbers(int[] numbers)
+void GetArrayRandomNumbers(int[] array)
 {
-    for(int i = 0; i < numbers.Length; i++)
+    for(int i = 0; i < array.Length; i++)
     {
-        numbers[i] = new Random().Next(100,1000);
+        array[i] = new Random().Next(100,1000);
     }
 }
-void PrintArray(int[] numbers)
+void PrintArray(int[] array)
 {
     Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
+    for(int i = 0; i < array.Length; i++)
     {
-        Console.Write(numbers[i] + " ");
+        Console.Write(array[i] + " ");
     }
     Console.Write("]");
     Console.WriteLine();
 }
-for (int i = 0; i < numbers.Length; i++)
-if (numbers[i] % 2 == 0)
+for (int i = 0; i < array.Length; i++)
+if (array[i] % 2 == 0)
 count++;
 {
-    Console.WriteLine($"всего {numbers.Length} чисел в массиве, {count} из них чётные");
+    Console.WriteLine($"всего {array.Length} чисел в массиве, {count} из них чётные");
 }
